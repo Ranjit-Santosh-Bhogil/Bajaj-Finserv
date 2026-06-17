c443d3424ba6c88eebd6345edfaba02e23222b4c
Ranjit Bhogil <bhogilranjit@gmail.com>
2026-06-17 16:24:01 +0530

    project initial

---
+++ src/main/java/com/ranjit/bajajfinservhealth/BajajFinservHealthApplication.java
@@ -0,0 +1,13 @@
+package com.ranjit.bajajfinservhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajFinservHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BajajFinservHealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ranjit/bajajfinservhealth/BajajFinservHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjit.bajajfinservhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajFinservHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
